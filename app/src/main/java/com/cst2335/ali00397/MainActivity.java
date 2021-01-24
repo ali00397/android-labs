bec813c898d94425431bcf8bde36fe94b44dbdd7
Hassan Ali <ali00397@algonquin.com>
2021-01-24 16:07:48 -0500

    Committing this with the message "step1"

--- app/src/main/java/com/cst2335/ali00397/MainActivity.java
+++ app/src/main/java/com/cst2335/ali00397/MainActivity.java
@@ -3,12 +3,56 @@ package com.cst2335.ali00397;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import static android.os.Build.VERSION_CODES.M;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linearlayout);
+        getResources().getString(R.string.toast_message);
+        TextView yourtext = findViewById(R.id.textView);
+        Button btn = findViewById(R.id.button);
+        CheckBox cb =  findViewById(R.id.checkBox);
+        cb.setChecked(true);
+        Switch cb1 = findViewById(R.id.switch1);
+        cb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton cb1, boolean isChecked) {
+
+               Snackbar.make(findViewById(R.id.button),"this pop message",Snackbar.LENGTH_LONG)
+                       .setAction("Undo", click ->cb1.setChecked(false));
+
+
+            }
+        });
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn.setText(R.string.toast_message);
+                Toast.makeText(getApplicationContext(),R.string.hello_message,Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+
+
     }
 }
