f51140d364d6f169974e6a5cfbd7440efadd92ae
Hassan Ali <ali00397@algonquin.com>
2021-02-06 17:14:18 -0500

    Commited for lab3

---
+++ app/src/main/java/com/cst2335/ali00397/Activity_Second.java
@@ -0,0 +1,6 @@
+package com.cst2335.ali00397;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_Second extends AppCompatActivity {
+}
--- app/src/main/java/com/cst2335/ali00397/MainActivity.java
+++ app/src/main/java/com/cst2335/ali00397/MainActivity.java
@@ -2,6 +2,8 @@ package com.cst2335.ali00397;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
@@ -19,37 +21,43 @@ import static android.os.Build.VERSION_CODES.M;
 public class MainActivity extends AppCompatActivity {
 
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences mypref = null;
+
+       String info = mypref.getString("important","initial String");
+        Intent nextpages = new Intent(this,Activity_Second.class);
+        nextpages.putExtra("EmailAdress","email");
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linearlayout);
-        getResources().getString(R.string.toast_message);
-        TextView yourtext = findViewById(R.id.textView);
-        Button btn = findViewById(R.id.button);
-        CheckBox cb =  findViewById(R.id.checkBox);
-        cb.setChecked(true);
-        Switch cb1 = findViewById(R.id.switch1);
-        cb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton cb1, boolean isChecked) {
+        setContentView(R.layout.activity_login_main);
+        {
 
-               Snackbar.make(findViewById(R.id.button),"this pop message",Snackbar.LENGTH_LONG)
-                       .setAction("Undo", click ->cb1.setChecked(false));
+            Button loginInfo = findViewById(R.id.button3);
+
+            loginInfo.setOnClickListener(v -> (View));
 
 
-            }
-        });
 
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                btn.setText(R.string.toast_message);
-                Toast.makeText(getApplicationContext(),R.string.hello_message,Toast.LENGTH_LONG).show();
 
-            }
-        });
+
+
+
+
+        };
+
+
+
+
+
 
 
 
