3030bd7fbcd97ab813d884526369814ae1b94ebf
Hassan Ali <ali00397@algonquin.com>
2021-02-06 20:12:58 -0500

    recent commit

--- app/src/main/java/com/cst2335/ali00397/MainActivity.java
+++ app/src/main/java/com/cst2335/ali00397/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,30 +20,50 @@ import com.google.android.material.snackbar.Snackbar;
 import static android.os.Build.VERSION_CODES.M;
 
 public class MainActivity extends AppCompatActivity {
+    EditText input;
 
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        SharedPreferences mypref = null;
+    SharedPreferences mypref = getSharedPreferences("EmailAddress",MODE_PRIVATE);
+        SharedPreferences.Editor editors = mypref.edit();
+        editors.putString("EmailAdress", input.getText().toString());
+        editors.commit();
 
-       String info = mypref.getString("important","initial String");
-        Intent nextpages = new Intent(this,Activity_Second.class);
-        nextpages.putExtra("EmailAdress","email");
+        input = findViewById(R.id.edit);
+        Button loginInfo = findViewById(R.id.button3);
+
+        loginInfo.setOnClickListener(clk-> onPause(input.getText().toString()));
+
+
+       // String savedinfo = mypref.getString("important","initial String");
+       // EditText input = findViewById(R.id.edit);
+       // input.getText(savedinfo);
+      //  Intent nextpages = new Intent(this,Activity_Second.class);
+      //  nextpages.putExtra("EmailAdress","email");
 
 
     }
+
+    private void onPause(String toString) {
+        SharedPreferences mypref = getSharedPreferences("EmailAddress",MODE_PRIVATE);
+        SharedPreferences.Editor editors = mypref.edit();
+        editors.putString("EmailAdress", input.getText().toString());
+        editors.commit();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_main);
+
+
+        onPause();
         {
-
-            Button loginInfo = findViewById(R.id.button3);
-
-            loginInfo.setOnClickListener(v -> (View));
+            
 
 
 
