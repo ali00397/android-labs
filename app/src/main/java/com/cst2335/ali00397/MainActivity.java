45798439309ab7cabb167a9cb2b866b4742a2b7d
Hassan Ali <ali00397@algonquinlive.com>
2021-02-14 14:47:49 -0500

    this file that are committed in my lab3

--- app/src/main/java/com/cst2335/ali00397/MainActivity.java
+++ app/src/main/java/com/cst2335/ali00397/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,15 +19,17 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
 
+    SharedPreferences mypref ;
     @Override
     protected void onPause() {
         super.onPause();
         Log.e(ACTIVITY_NAME, "In function: onPaused ran without any issues");
 
 
-        SharedPreferences mypref = getSharedPreferences("EmailAddress", MODE_PRIVATE);
+       mypref = getSharedPreferences("EmailAddress", MODE_PRIVATE);
         String savedText = mypref.getString("important","default string");
         EditText inputText = findViewById(R.id.edit);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         inputText.setText(savedText);
         Button loginInfo = findViewById(R.id.button3);
         loginInfo.setOnClickListener(clk -> onPause(inputText.getText().toString()));
@@ -34,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onPause(String savedInfo) {
-        SharedPreferences mypref = getSharedPreferences("EmailAddress", MODE_PRIVATE);
+
         SharedPreferences.Editor editors = mypref.edit();
         editors.putString("EmailAddress", savedInfo);
         editors.commit();
@@ -46,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_main);
+
         Log.e(ACTIVITY_NAME, "In function: onCreate ran without any issues");
 
         Intent goToprofile = new Intent(this, ProfileActivity.class);
