79db79f2013805cf566a048d8009e25dcbec1be6
Hassan Ali <ali00397@algonquinlive.com>
2021-03-11 11:49:48 -0500

    finished committing lab05

--- app/src/main/java/com/cst2335/ali00397/WeatherForecast.java
+++ app/src/main/java/com/cst2335/ali00397/WeatherForecast.java
@@ -37,14 +37,17 @@ public class WeatherForecast extends AppCompatActivity {
 
         ForecastQuery qr = new ForecastQuery();
         qr.execute("http://api.openweathermap.org/data/2.5/weather?q=ottawa,ca&APPID=7e943c97096a9784391a981c4d878b22&mode=xml&units=metric");
-        myForecast cast = new myForecast();
-        cast.execute("http://api.openweathermap.org/data/2.5/uvi?appid=7e943c97096a9784391a981c4d878b22&lat=45.348945&lon=-75.759389");
-        myBitmapAsync bt = new myBitmapAsync();
-        bt.execute("https://openweathermap.org/img/w/01d.png");
+
     }
 
 
  private class ForecastQuery extends AsyncTask<String,Integer,String> {
+        private String uv;
+        private String min;
+        private String max;
+        private String current;
+        private Bitmap image;
+
 
 
      @Override
@@ -86,10 +89,10 @@ public class WeatherForecast extends AppCompatActivity {
                      {
                          //If you get here, then you are pointing to a <Weather> start tag
 
-                         String outlook = xpp.getAttributeValue(null,    "min");
-                         String windy = xpp.getAttributeValue(null, "max");
-                         String value = xpp.getAttributeValue(null,"value");
-                     }else{
+                        min = xpp.getAttributeValue(null,    "min");
+                         max = xpp.getAttributeValue(null, "max");
+                          current = xpp.getAttributeValue(null,"value");
+                     }else if(){
                          eventType = xpp.next(); //move to the next xml event and store it in a variable
 
                      }
@@ -124,136 +127,6 @@ public class WeatherForecast extends AppCompatActivity {
  }
 
 
- public class myForecast extends AsyncTask<String,Integer,String>{
-
-
-     @Override
-     protected String doInBackground(String... args) {
-
-         try {
-
-             URL url = new URL(args[0]);
-             //open the connection
-             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-             //wait for data:
-             InputStream response = urlConnection.getInputStream();
-
-             //JSON reading:   Look at slide 26
-             //Build the entire string response:
-             BufferedReader reader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
-             StringBuilder sb = new StringBuilder();
-
-             String line = null;
-             while ((line = reader.readLine()) != null)
-             {
-                 sb.append(line + "\n");
-             }
-             String result = sb.toString(); //result is the whole string
-
-
-             // convert string to JSON: Look at slide 27:
-             JSONObject jObject = new JSONObject(result);
-
-             //get the float associated with "value"
-             float value = (float) jObject.getDouble("value");
-
-             Log.i("MainActivity", "The uv is now: " + value) ;
-
-         }catch (Exception e){
-         }
-         publishProgress(25);
-         publishProgress(50);
-         publishProgress(75);
-         return "reply";
-
-     }
-
-
-     public void onProgressUpdate(Integer...args){
-
-
-     }
-
-
-     public void onPostExecute(String fromDoInBackgrounds){
-
-
-     }
-
-
-
- }
-
- private Bitmap bitmap = null;
- private  Bitmap image = null;
- private String urlString = null;
-
-
-
- public class myBitmapAsync extends AsyncTask<String, Integer, String> {
-
-
-
-
-     @Override
-     protected String doInBackground(String... arg) {
-
-         try {
-
-             HttpURLConnection connection;
-             URL url = new URL(urlString);
-             connection = (HttpURLConnection) url.openConnection();
-             connection.connect();
-             int responseCode = connection.getResponseCode();
-             if (responseCode == 200) {
-                 image = BitmapFactory.decodeStream(connection.getInputStream());
-
-
-             }
-
-             FileOutputStream outputStream = openFileOutput( "01d" + ".png", Context.MODE_PRIVATE);
-             image.compress(Bitmap.CompressFormat.PNG, 80, outputStream);
-             outputStream.flush();
-             outputStream.close();
-
-
-
-         }catch (Exception e){
-
-
-
-         }
-         publishProgress(25);
-         publishProgress(50);
-         publishProgress(75);
-
-
-         return "png";
-     }
-
-     public boolean fileExistance(String fname){
-         File file = getBaseContext().getFileStreamPath(fname);
-         return file.exists();
-     }
-
-
-
-     public void onProgressUpdate(Integer... arg){
-         ProgressBar progressBar = findViewById(R.id.weather);
-         progressBar.setVisibility(View.VISIBLE);
-         progressBar.setVisibility(arg[0]);
-
-
-     }
-
-
-
-     public void onPostExecute(String fromDoInBackground){
-         ProgressBar progressBar = findViewById(R.id.weather);
-         progressBar.setVisibility(View.INVISIBLE);
-         Log.e("ForecastQuery",fromDoInBackground);
-
-     }
+ 
  }
 }
